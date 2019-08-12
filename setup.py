511d25c9061f912dea28daf9f51a462f8d395bb7
Marc Maxmeister <mmaxmeister@lifeegx.com>
2019-08-12 15:28:40 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,48 @@
+# Lib
+from setuptools import setup, find_packages
+
+setup(
+    name='methylize',
+    version='0.2.0',
+    description='Analysis software for Illumina methylation arrays',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    project_urls = {
+        "Documentation": "https://life-epigenetics-methylize.readthedocs-hosted.com/en/latest/",
+        "Source": "https://github.com/lifeEGX/methylize/",
+        "Funding": "https://lifeegx.com/"
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Medical Science Apps.',
+        'Framework :: Jupyter',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+      ],
+    keywords='analysis methylation dna data processing life epigenetics illumina parallelization',
+    url='https://github.com/LifeEGX/methylize',
+    license='MIT',
+    author='Life Epigenetics',
+    author_email='info@lifeegx.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'statsmodels'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points='''
+        [console_scripts]
+        methylize-cli=methylize.cli:app
+    ''',
+)
