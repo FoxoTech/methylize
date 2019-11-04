b6df188af5bd5b3f6c69b10e372767e006726ab7
marcmaxson <marcmaxmeister@gmail.com>
2019-11-04 16:31:28 -0700

    v0.9.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='methylize',
-    version='0.9.1',
+    version='0.9.2',
     description='Analysis software for Illumina methylation arrays',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
