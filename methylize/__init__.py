feed18659f1ec69fbc5be367ee518be2ec851fa0
Marc Maxmeister <mmaxmeister@foxotechnologies.com>
2021-08-05 10:13:39 -0600

    fixed probe2chr loading bug; removed some unused redundant functions already in methylcheck

--- methylize/__init__.py
+++ methylize/__init__.py
@@ -1,7 +1,6 @@
 import logging
 from .diff_meth_pos import diff_meth_pos, volcano_plot, manhattan_plot
-from .helpers import load, load_both
 
 logging.basicConfig(level=logging.INFO)
 
-__all__ = ['diff_meth_pos', 'volcano_plot', 'manhattan_plot', 'load', 'load_both']
+__all__ = ['diff_meth_pos', 'volcano_plot', 'manhattan_plot']
